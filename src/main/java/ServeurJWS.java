e8eee0f8aa77962fb20fd0d399746d5e258b2e63
IssamBen98 <benatik.issam@gmail.com>
2022-10-26 22:52:41 +0100

    Creation des operations services web et du serveur web

---
+++ src/main/java/ServeurJWS.java
@@ -0,0 +1,12 @@
+import jakarta.xml.ws.* ;
+import ws.*;
+
+public class ServeurJWS
+{
+    public static void main(String[] args)
+    {
+        // 0.0.0.0 : adresse IP quelconque appartenant au même reseau local du serveur web
+     Endpoint.publish("http://0.0.0.0:9191/",new BanqueService());
+     System.out.println("Web service deploye sur 0.0.0.0:9191");
+    }
+}
