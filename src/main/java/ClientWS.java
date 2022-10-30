a783e30e416ce523d72a504fd9ba80b0d1b2d662
IssamBen98 <benatik.issam@gmail.com>
2022-10-30 23:58:55 +0100

    CLientJava

---
+++ src/main/java/ClientWS.java
@@ -0,0 +1,18 @@
+import proxy.BanqueService;
+import proxy.Bws;
+import proxy.Compte;
+
+import java.util.List;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService stub = new Bws().getBanqueServicePort();
+        System.out.println("--------- Convert Methode --------");
+        System.out.println("999 Euro is " + stub.convert(999) + " Mad");
+        System.out.println("--------- GetComptes Methode --------");
+        List<Compte> list = stub.getComptes();
+        list.forEach(c -> {
+            System.out.println("Compte : " + c.getCode() + " => " + c.getSolde() + " DHS");
+        });
+    }
+}
