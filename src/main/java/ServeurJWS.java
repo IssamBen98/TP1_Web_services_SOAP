303b1bab4deeb56ca686d3681b39b1630169c8d1
IssamBen98 <benatik.issam@gmail.com>
2022-10-31 00:02:56 +0100

    update

--- src/main/java/ServeurJWS.java
+++ src/main/java/ServeurJWS.java
@@ -6,7 +6,7 @@ public class ServeurJWS
     public static void main(String[] args)
     {
         // 0.0.0.0 : adresse IP quelconque appartenant au même reseau local du serveur web
-     Endpoint.publish("http://0.0.0.0:9191/",new BanqueService());
-     System.out.println("Web service deploye sur 0.0.0.0:9191");
+     Endpoint.publish("http://0.0.0.0:9092/",new BanqueService());
+     System.out.println("Web service deploye sur 0.0.0.0:9092");
     }
 }
